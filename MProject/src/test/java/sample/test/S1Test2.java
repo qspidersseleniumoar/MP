bbbfe28d493e11aa97bb86d9b6a97ebd749c853a
deepak <deepak@deepak-PC>
2018-09-28 17:07:44 +0530

    commit 2 test

---
+++ MProject/src/test/java/sample/test/S1Test2.java
@@ -0,0 +1,16 @@
+package sample.test;
+
+import org.testng.annotations.Test;
+
+public class S1Test2 {
+	
+	@Test
+	public void test3() {
+		System.out.println("execute test3");
+	}
+	@Test
+	public void test4() {
+		System.out.println("execute test4");
+	}
+
+}
