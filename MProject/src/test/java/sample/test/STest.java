6ff8beeebc008f283bd08113ce87229259465d62
deepak <deepak@deepak-PC>
2018-09-28 16:46:56 +0530

    commit

---
+++ MProject/src/test/java/sample/test/STest.java
@@ -0,0 +1,16 @@
+package sample.test;
+
+import org.testng.annotations.Test;
+
+public class STest {
+	
+	@Test
+	public void test1() {
+		System.out.println("execute test1");
+	}
+	@Test
+	public void test2() {
+		System.out.println("execute test2");
+	}
+
+}
